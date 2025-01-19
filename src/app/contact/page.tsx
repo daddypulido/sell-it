9ddadceec46c6d8317d252daa8ee096834d85714
Alexander Pulido <Alexanderpulido10@gmail.com>
2025-01-18 21:53:20 -0800

    created home page, shop and contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact(){
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Contact us at Alexanderpulido10@gmail.com</p>
+            
+        </div>
+    )
+}
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,21 @@
+export default function Shop() {
+    return (
+        <div>
+            <h1>Shop Our Custom Products!</h1>
+            <div>
+                <div>
+                    <h2>BCV OG Banner</h2>
+                    <p>Price: $25</p>
+                    <button>Add to Cart</button>
+                </div>
+                <div>
+                    <h2>BCV Graffiti Logo</h2>
+                    <p>Price:$25</p>
+                    <button>Add to Cart</button>
+                </div>
+
+            </div>
+            
+        </div>
+    );
+}
